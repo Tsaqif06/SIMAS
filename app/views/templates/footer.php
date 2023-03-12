fffd199c646884f4c67583d3abc736e5184e01e6
Fauzi002 <mfauzialvarizi@gmail.com>
2023-03-12 19:49:46 +0700

    Menyelesaikan halaman pada home/index Fixed

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -2,7 +2,7 @@
         <!-- partial:partials/_footer.html -->
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©2023 WEB Dev SIMAS  </a></span>
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block"><a>Copyright ©2023 WEB Dev SIMAS  </a></span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
           </div>
         </footer>
