378bfae1b7f2042c81f9c88c914e61a0df9a437a
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-03-27 10:09:51 +0700

    Membuat Page Kompetensi Keahlian Pada Menu Maste

---
+++ app/models/Master_models/Kompkeahlian_model.php
@@ -0,0 +1,76 @@
+<?php
+
+class Kompkeahlian_model
+{
+    private $table = 'masterkompetensikeahlian';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function getAllData()
+    {
+        $this->db->query("SELECT * FROM {$this->table}");
+        return $this->db->fetchAll();
+    }
+
+    public function getDataById($id)
+    {
+        $this->db->query("SELECT * FROM {$this->table} WHERE id_kompkeahlian = :id");
+        $this->db->bind("id", $id);
+        return $this->db->fetch();
+    }
+
+    public function tambahData($data)
+    {
+        $this->db->query(
+            "INSERT INTO {$this->table}
+                VALUES 
+            (null, :kode_kompkeahlian, :nama_kompkeahlian)"
+        );
+
+        $this->db->bind('kode_kompkeahlian', $data['kode_kompkeahlian']);
+        $this->db->bind('nama_kompkeahlian', $data['nama_kompkeahlian']);
+
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function hapusData($id)
+    {
+        $this->db->query("DELETE FROM {$this->table} WHERE id_kompkeahlian = :id");
+        $this->db->bind("id", $id);
+
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function ubahData($data)
+    {
+        $this->db->query(
+            "UPDATE {$this->table}
+                SET 
+                kode_kompkeahlian = :kode_kompkeahlian,
+                nama_kompkeahlian = :nama_kompkeahlian
+                WHERE id_kompkeahlian = :id"
+        );
+
+        $this->db->bind('kode_kompkeahlian', $data['kode_kompkeahlian']);
+        $this->db->bind('nama_kompkeahlian', $data['nama_kompkeahlian']);
+        $this->db->bind('id', $data['id_kompkeahlian']);
+
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function cariData()
+    {
+        $keyword = $_POST['keyword'];
+
+        $this->db->query("SELECT * FROM {$this->table} WHERE nama_kompkeahlian LIKE :keyword");
+        $this->db->bind("keyword", "%$keyword%");
+        return $this->db->fetchAll();
+    }
+}
---
+++ app/views/master/kompkeahlian.php
@@ -0,0 +1,119 @@
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12 grid-margin">
+                <div class="row">
+                    <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                        <h3 class="font-weight-bold">DATA KOMPETENSI KEAHLIAN</h3>
+                        <h6 class="font-weight-normal mb-0">WEB DEV | SIMAS</h6>
+                    </div>
+                    <div class="col-12 col-xl-4">
+                        <div class="justify-content-end d-flex">
+                            <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
+                                <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                    <i class="mdi mdi-calendar"></i> Hari ini (11 Mar 2023)
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
+                                    <a class="dropdown-item" href="#">January - March</a>
+                                    <a class="dropdown-item" href="#">March - June</a>
+                                    <a class="dropdown-item" href="#">June - August</a>
+                                    <a class="dropdown-item" href="#">August - November</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <?php Flasher::flash(); ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <button type="button" class="btn btn-primary my-3 tampilModalTambah" data-bs-toggle="modal" data-bs-target="#modal">
+                    Tambah Data Kompetensi Keahlian
+                </button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-main">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Aksi</th>
+                                        <th>Kode Kompetensi Keahlian</th>
+                                        <th>Nama Kompetensi Keahlian</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $i = 1 ?>
+                                    <?php foreach ($data['kompkeahlian'] as $row) : ?>
+                                        <tr>
+                                            <td><?= $i++ ?></td>
+                                            <td class="font-weight-medium">
+                                                <a href="" class="badge text-bg-success tampilModalUbahKompkeahlian" style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#modal" data-id="<?= $row['id_kompkeahlian'] ?>">
+                                                    Edit
+                                                </a>
+                                                <a href="<?= BASEURL ?>master/hapusData/kompkeahlian/<?= $row['id_kompkeahlian'] ?>"">
+                                                    <div class=" font-weight-medium pt-2">
+                                                    <div class="badge badge-danger delete" onclick="return confirm('Apakah Anda Yakin Mau Menghapus Data?')">Delete</div>
+                                                </a>
+                                            </td>
+                                            <td><?= $row["kode_kompkeahlian"]; ?></td>
+                                            <td><?= $row["nama_kompkeahlian"] ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="modalLabel">Tambah Data Kompetensi Keahlian</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="<?= BASEURL ?>master/tambahData/Kompkeahlian" method="post">
+                            <input type="text" name="id_kompkeahlian" id="id_kompkeahlian" value="<?= $row['id_kompkeahlian'] ?>" style="display: none;">
+                            <div class="mb-3">
+                                <label for="kode_kompkeahlian" class="form-label">Kode Kompetensi Keahlian</label>
+                                <input type="text" class="form-control" name="kode_kompkeahlian" id="kode_kompkeahlian" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="nama_kompkeahlian" class="form-label">Nama Kompetensi Keahlian</label>
+                                <input type="text" class="form-control" name="nama_kompkeahlian" id="nama_kompkeahlian" required>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Tambah Data</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<script src="../js/script/kompkeahlian.js"></script>
