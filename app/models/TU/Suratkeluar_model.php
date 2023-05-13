42a2b5131814edcf4f39bb8d9665c4a6af237337
Tsaqif06 <tsaqifcakep87@gmail.com>
2023-05-13 14:27:19 +0700

    Fix Header Location Import Data TU

--- app/models/TU/Suratkeluar_model.php
+++ app/models/TU/Suratkeluar_model.php
@@ -98,7 +98,7 @@ class Suratkeluar_model
         // Cek file diupload apa belum
         if (!isset($_FILES['file']['name'])) {
             Flasher::setFlash('Error', 'Harap pilih file Excel terlebih dahulu', 'danger');
-            header('location: ' . BASEURL . '/siswa');
+            header('location: ' . BASEURL . '/suratkeluar');
             exit;
         }
 
