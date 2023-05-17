ca869ea8f0dcc198447397c05b6cbc59391387d0
Tsaqif06 <tsaqifcakep87@gmail.com>
2023-05-17 09:28:35 +0700

    Fix Bug

--- app/views/humas/pkl/rekap/penempatan/index.php
+++ app/views/humas/pkl/rekap/penempatan/index.php
@@ -5,8 +5,13 @@
             <div class="col-md-12 grid-margin">
               <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h3 class="font-weight-bold">DATA PENEMPATAN</h3>
-                  <h6 class="font-weight-normal mb-0">Laman Data Penempatan Siswa PKL | <span class="text-primary">SIMAS</span></h6>
+                  <h3 class="font-weight-bold">PENEMPATAN PKL</h3>
+                  <h6 class="font-weight-normal mb-0"><a class="text-dark" href="<?= BASEURL; ?>/pkl"> Laman
+                      PKL</a> | <span class="text-primary"> penempatan </span></h6>
+                </div>
+                <div class="col-12 col-xl-4">
+                  <div class="justify-content-end d-flex">
+                  </div>
                 </div>
               </div>
             </div>
@@ -17,7 +22,7 @@
               <div class="card">
                 <div class="card-body">
                   <div class="card data-icon-card-primary">
-                    <div classtw="card-body">
+                    <div class="card-body">
                       <div class="row">
                         <div class="col-12 text-white">
                           <h3>Teknik Grafika</h3>
@@ -55,142 +60,147 @@
                   </div>
                 </div>
               </div>
-
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="card data-icon-card-primary">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-12 text-white">
-                            <h3>Teknik Mekatronika</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_MEKA_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
-                            ini</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="card data-icon-card-primary">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-12 text-white">
-                            <h3>Rekayasa Perangkat Lunak</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_RPL_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
-                            ini</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="card data-icon-card-primary">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-12 text-white">
-                            <h3>Teknik Komputer Jaringan</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_TKJ_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
-                            ini</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="card data-icon-card-primary">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-12 text-white">
-                            <h3>Desain Komunikasi Visual</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_DKV_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
-                            ini</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="card data-icon-card-primary">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-12 text-white">
-                            <h3>Perhotelan</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_PH_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
-                            ini</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="card data-icon-card-primary">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-12 text-white">
-                            <h3>Animasi</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_ANI_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
-                            ini</button></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
             </div>
 
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card data-icon-card-primary">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 text-white">
+                          <h3>Teknik Mekatronika</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_MEKA_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
+                          ini</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card data-icon-card-primary">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 text-white">
+                          <h3>Rekayasa Perangkat Lunak</h3>
+                        </div>
+                      </div>
+                    </div>
 
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_RPL_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
+                          ini</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card data-icon-card-primary">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 text-white">
+                          <h3>Teknik Komputer Jaringan</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_TKJ_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
+                          ini</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card data-icon-card-primary">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 text-white">
+                          <h3>Desain Komunikasi Visual</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_DKV_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
+                          ini</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card data-icon-card-primary">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 text-white">
+                          <h3>Perhotelan</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_PH_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
+                          ini</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card data-icon-card-primary">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 text-white">
+                          <h3>Animasi</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                      <a href="<?= BASEURL; ?>/pkl/penempatan&kelas=XI_ANI_A"><button type="button" class="btn btn-inverse-primary btn-fw btn-block">Masuk ke laman
+                          ini</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
           </div>
+        </div>
+
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023. SIMAS.
+              All rights reserved.</span>
+          </div>
+        </footer>
