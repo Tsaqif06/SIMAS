8aaadd1f56de371dab464044b714ba212ecf29b2
fami0110 <masandofami@gmail.com>
2023-05-21 18:39:25 +0700

    wkwkkwkwkwkk

--- app/views/humas/bkk/loker/form.php
+++ app/views/humas/bkk/loker/form.php
@@ -6,21 +6,7 @@
                     <h3 class="font-weight-bold">LOWONGAN KERJA</h3>
                     <h6 class="font-weight-normal mb-0">WEB DEV | SIMAS</h6>
                 </div>
-                <div class="col-12 col-xl-4">
-                    <div class="justify-content-end d-flex">
-                        <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                            <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                <i class="mdi mdi-calendar"></i> Hari ini (11 Mar 2023)
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                                <a class="dropdown-item" href="#">January - March</a>
-                                <a class="dropdown-item" href="#">March - June</a>
-                                <a class="dropdown-item" href="#">June - August</a>
-                                <a class="dropdown-item" href="#">August - November</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
