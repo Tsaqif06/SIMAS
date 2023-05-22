10616d33c5b6c151f7a1aa21eb082470ea4902f1
Tsaqif06 <tsaqifcakep87@gmail.com>
2023-05-22 08:49:45 +0700

    Fix Notif TU

--- public/js/script/notifikasi.js
+++ public/js/script/notifikasi.js
@@ -5,13 +5,13 @@ let notification = {
 			notif.remove();
 		}
 	},
-	add: function (title, content, href = '', icon = "ti-email") {
+	add: function (title, content, href = '', icon = ["bg-info", "ti-email"]) {
 		// Create and append the element
 		$("#notification .dropdown-menu").append(`
       <a ${href == '' ? '' : `href="${href}" `}class="dropdown-item preview-item">
         <div class="preview-thumbnail">
-          <div class="preview-icon bg-info">
-            <i class="${icon} mx-0"></i>
+          <div class="preview-icon ${icon[0]}">
+            <i class="${icon[1]} mx-0"></i>
           </div>
         </div>
         <div class="preview-item-content">
@@ -27,22 +27,25 @@ let notification = {
 
 $(document).ready(function () {
 	setInterval(function () {
+		console.clear();
 		$.ajax({
 			url: "http://localhost/SIMAS/public/notification",
-			data: { method : "get_pengajuan" },
+			data: { get_notification : true },
 			method: "post",
 			dataType: "json",
 			success: function (data) {
 				notification.clear();
-				if (parseInt(data) == 0) {
+
+				data.forEach(notif => {
+					if (notif[0] != 0) {
+						notification.add(notif[1], notif[2], notif[3], notif[4]);
+					}
+				});
+
+				if ($('#notification .dropdown-item').length == 0) {
 					$("#notification .count").addClass("d-none");
 				} else {
 					$("#notification .count").removeClass("d-none");
-					notification.add(
-						"Pengajuan Surat Baru",
-						`${data} surat baru sedang diajukan`,
-						"http://localhost/SIMAS/public/suratpengajuan"
-					);
 				}
 			},
 		});
