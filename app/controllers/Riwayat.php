d8859a9cb8ee92347027f0ed2ea1f9339f92fb0a
Tsaqif06 <tsaqifcakep87@gmail.com>
2023-05-23 09:08:18 +0700

    Fix hak akses HUMAS

--- app/controllers/Riwayat.php
+++ app/controllers/Riwayat.php
@@ -21,12 +21,12 @@ class Riwayat extends Controller
             $this->view('templates/header', $data);
             $this->view('riwayat/index', $data);
             $this->view('riwayat/info', $data);
-            $this->view('templates/footer');
+            $this->view('templates/footerwm');
         } else {
             $this->view('templates/header', $data);
             $this->view('riwayat/detail', $data);
             $this->view('riwayat/info', $data);
-            $this->view('templates/footer');
+            $this->view('templates/footerwm');
         }
     }
 
